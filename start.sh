8e0d78da8fd576390a5cf672dac3851621cbbceb
Julian Acosta <juliandavidacosta2018@gmail.com>
2025-08-04 12:31:31 -0500

    Final?

--- start.sh
+++ start.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
+
+set -e  # Si ocurre un error, se detiene el script inmediatamente
+
+# Instalar los navegadores necesarios para Playwright
 playwright install --with-deps
+
+# Ejecutar el script principal
 python main.py
